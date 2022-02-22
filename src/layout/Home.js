bd9d8b9c5ef541ecdf27d66bc24e474cf9b00fe6
Andrey Babkin <babkin.andrey@gmail.com>
2022-02-22 18:00:06 +0300

    added expenses chart

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
-// import Container from 'react-bootstrap/Container';
 import InsertionForm from '../components/InsertionForm';
 import TotalBalance from '../components/TotalBalance';
+import ExpensesPie from '../components/ExpensesPie';
 
 function Home() {
   const [records, setRecords] = useState([]);
@@ -31,11 +31,14 @@ function Home() {
   return (
     <Container>
       <Row>
-        <Col sm={12} md={6} lg={4} className="mt-4 ps-3 pe-3 order-2 order-md-1">
+        <Col sm={12} lg={4} className="mt-4 ps-3 pe-3 order-2 order-md-1">
           <InsertionForm addIncome={addIncome} addExpense={addExpense} />
         </Col>
-        <Col sm={12} md={6} lg={4} className="mt-4 ps-3 pe-3 order-1 order-md-2">
+        <Col sm={12} lg={4} className="mt-4 ps-3 pe-3 order-1 order-md-2">
           <TotalBalance />
+        </Col>
+        <Col sm={12} lg={4} className="mt-4 ps-3 pe-3 order-1 order-md-2">
+          <ExpensesPie />
         </Col>
       </Row>
     </Container>
